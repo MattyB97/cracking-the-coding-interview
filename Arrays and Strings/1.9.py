3bb0a6ec90b1a3a882f71fcd81df068f49036841
Matthew Boivin <mattcarterboivin@gmail.com>
2020-12-31 01:12:19 -0500

    Solved 1.9
    Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column are set to 0

---
+++ Arrays and Strings/1.9.py
@@ -0,0 +1,15 @@
+# String Rotation: Check if string s2 is contained in s1
+# Time Complexity: O(n) where n = s1
+# Space Complexity: O(n) where n = s1
+
+def is_substring(s1: str, s2: str) -> str:
+    """Returns True if s2 is a rotation of s1
+>>> is_substring('waterbottle', 'erbottlewat')
+True
+>>> is_substring('hello', 'world')
+False"""
+    if len(s1) != len(s2):
+        return False
+    s1 += s1
+    return s2 in s1
+
