f5fb95f8e320f0671405f395873a0a64e7e39e36
Matthew Boivin <mattcarterboivin@gmail.com>
2021-01-02 20:22:25 -0500

    2.3 Delete Middle Node Solved
    Implement an algorithm to delete a middle node. Note that middle node simply means not head or tail node

---
+++ Linked_Lists/2.3.py
@@ -0,0 +1,34 @@
+# Delete Middle Node: Implement an algorithm to delete a middle node
+# Note that middle node simply means not head or tail node
+
+from Linked_Lists.Linked_List import Node
+from Linked_Lists.Linked_List import SLinkedList
+
+
+def delete_middle_node(list: SLinkedList, node: Node) -> SLinkedList:
+    if list.head is node:
+        list.head = list.head.next
+        return list
+    cur_node = list.head
+    while cur_node.next is not None:
+        if cur_node.next == node:
+            cur_node.next = node.next
+            return list
+        cur_node = cur_node.next
+    return
+
+# Driver Code
+# list = SLinkedList()
+# list.head = Node(1)
+# n1 = Node(1)
+# n2 = Node(3)
+# n3 = Node(4)
+# n4 = Node(3)
+# list.head.next = n1
+# n1.next = n2
+# n2.next = n3
+# n3.next = n4
+# list.list_print()
+# print()
+# list = delete_middle_node(list, list.head)
+# list.list_print()
