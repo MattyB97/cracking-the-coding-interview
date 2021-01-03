88d8b4d4222e7471611dc561bfb6a370b245adc0
Matthew Boivin <mattcarterboivin@gmail.com>
2021-01-02 20:23:36 -0500

    Commented out driver code

--- Linked_Lists/2.1.py
+++ Linked_Lists/2.1.py
@@ -22,18 +22,18 @@ def remove_duplicates(list: SLinkedList) -> SLinkedList:
         slow_pointer = slow_pointer.next
     return list
 
-
-list = SLinkedList()
-list.head = Node(1)
-n1 = Node(1)
-n2 = Node(3)
-n3 = Node(4)
-n4 = Node(3)
-list.head.next = n1
-n1.next = n2
-n2.next = n3
-n3.next = n4
-list.list_print()
-list = remove_duplicates(list)
-print()
-list.list_print()
+# Driver Code
+# list = SLinkedList()
+# list.head = Node(1)
+# n1 = Node(1)
+# n2 = Node(3)
+# n3 = Node(4)
+# n4 = Node(3)
+# list.head.next = n1
+# n1.next = n2
+# n2.next = n3
+# n3.next = n4
+# list.list_print()
+# list = remove_duplicates(list)
+# print()
+# list.list_print()
--- Linked_Lists/2.2.py
+++ Linked_Lists/2.2.py
@@ -22,17 +22,17 @@ def kth_to_last_pointers(list: SLinkedList, index: int) -> Node:
         else:
             slow = slow.next
 
-
-list = SLinkedList()
-list.head = Node(1)
-n1 = Node(1)
-n2 = Node(3)
-n3 = Node(4)
-n4 = Node(3)
-list.head.next = n1
-n1.next = n2
-n2.next = n3
-n3.next = n4
-list.list_print()
-print()
-print(kth_to_last_pointers(list, 3).data)
+# Driver Code
+# list = SLinkedList()
+# list.head = Node(1)
+# n1 = Node(1)
+# n2 = Node(3)
+# n3 = Node(4)
+# n4 = Node(3)
+# list.head.next = n1
+# n1.next = n2
+# n2.next = n3
+# n3.next = n4
+# list.list_print()
+# print()
+# print(kth_to_last_pointers(list, 3).data)
